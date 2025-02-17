c27ce23e81c7de6db6e0466082333dd5f6e22cd5
Henrique Cerian <46903418+JW26T-Prj@users.noreply.github.com>
2025-02-16 21:51:18 -0300

    Update toca para o último vivo.lua

--- toca para o último vivo.lua
+++ toca para o último vivo.lua
@@ -1,18 +1,17 @@
 function eventPlayerDied(name)
-		local i=0
-		local name
-		for pname,player in next,tfm.get.room.playerList do
-			if not player.isDead then
-				i=i+1
-				name=pname
-			end
+	local i=0
+	local name
+	for pname,player in next,tfm.get.room.playerList do
+		if not player.isDead then
+			i=i+1
+			name=pname
 		end
-		if i==0 then
-			active=-1
-		elseif i==1 then
-			active=-1
-			tfm.exec.giveCheese(name)
-			tfm.exec.playerVictory(name)
-			tfm.exec.setGameTime(10)
+	end
+	if i==0 then
+		-- não fazer nada
+	elseif i==1 then
+		tfm.exec.giveCheese(name)
+		tfm.exec.playerVictory(name)
+		tfm.exec.setGameTime(10)
 	end
 end
